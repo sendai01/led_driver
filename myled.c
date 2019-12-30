af29f936ed680a8903d47e42b4b443a90ce789a6
sendai01 <user.name>
2019-12-30 16:42:59 +0000

    second commit

--- myled.c
+++ myled.c
@@ -5,7 +5,7 @@
 #include <linux/uaccess.h>
 #include <linux/io.h>
 
-MODULE_AUTHOR("Ryuichi Ueda, Kazuki Kawahara");
+MODULE_AUTHOR("Kazuki Kawahara, Ryuichi Ueda");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
